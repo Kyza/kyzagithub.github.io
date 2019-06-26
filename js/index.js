621da45ee34ee8cd15817d1cee0054d61d6ea43c
KyzaGitHub <tylerc2077@gmail.com>
2019-06-26 14:31:43 -0400

    Update

--- js/index.js
+++ js/index.js
@@ -14,4 +14,15 @@ setInterval(() => {
     document.getElementById("navbarButtons").className = "collapse navbar-collapse";
     document.getElementById("navbar-toggle").style = "";
   }
+
+  // Make sure all notification images have alts.
+  var cr8 = document.getElementsByTagName("crate")[0].childNodes[0].shadowRoot;
+  var images = cr8.querySelector(".notifications").querySelectorAll("img");
+  for (var i = 0; i < images.length; i++) {
+    if (images[i].alt.toString().length < 1) {
+      images[i].alt = "image";
+    }
+  }
+  // Make sure the iframe has a title.
+  cr8.querySelector("iframe").title = "DisWeb Crate";
 }, 100);
